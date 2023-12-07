b25c9868964e5092a45b348e98ef8060187c2448
Dyoish <joshdalmacio19@gmail.com>
2023-12-07 19:05:52 +0800

    Second Commit

--- app/src/main/java/com/dalmacio/joshalfred/block3/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/dalmacio/joshalfred/block3/p1/quiz/MainActivity.kt
@@ -2,10 +2,13 @@ package com.dalmacio.joshalfred.block3.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun boardTapped(view: View) {}
 }
